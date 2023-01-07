1d8c616bcc7097afeca1b1a8e4519e163744d9fb
KuraiNT <cho71167@gmail.com>
2023-01-07 16:08:00 +0700

    validation form user

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './LoginForm.scss';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -12,6 +13,8 @@ function LoginForm() {
     const [showPassword, setShowPassword] = useState(false);
     const [typePassword, setTypePassword] = useState('password');
 
+    const [validated, setValidated] = useState(false);
+
     const handleShowPassword = () => {
         setShowPassword(true);
         setTypePassword('text');
@@ -22,27 +25,56 @@ function LoginForm() {
         setTypePassword('password');
     };
 
+    const handleValidated = (e) => {
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+        setValidated(true);
+    };
+
     return (
-        <Form>
-            <Form.Group className={cx('modal_user-body')}>
+        <Form noValidate validated={validated} onSubmit={handleValidated}>
+            <Form.Group
+                controlId="validationCustomEmail"
+                className={cx('modal_user-body')}
+            >
                 <Form.Label className={cx('modal_user-label')}>
                     Email
                 </Form.Label>
                 <Form.Control
+                    required
+                    aria-describedby="inputGroupPrepend"
                     className={cx('modal_user-input')}
                     type="email"
                     placeholder="exmple@mail.com"
                 />
+                <Form.Control.Feedback
+                    type="invalid"
+                    className={cx('mes-error')}
+                >
+                    Email is required
+                </Form.Control.Feedback>
             </Form.Group>
-            <Form.Group className={cx('modal_user-body')}>
+            <Form.Group
+                className={cx('modal_user-body', 'modal_user-body--position')}
+            >
                 <Form.Label className={cx('modal_user-label')}>
                     Password
                 </Form.Label>
                 <Form.Control
+                    required
                     className={cx('modal_user-input')}
                     type={typePassword}
                     placeholder="*********"
                 />
+                <Form.Control.Feedback
+                    type="invalid"
+                    className={cx('mes-error')}
+                >
+                    Password is required
+                </Form.Control.Feedback>
                 {showPassword ? (
                     <Button
                         onClick={handleHidePassword}
