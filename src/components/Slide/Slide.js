b10acfd0927fa894bfef8e45e42bc7d3f30e9cd4
KuraiNT <cho71167@gmail.com>
2023-01-10 15:18:21 +0700

    slide show

---
+++ src/components/Slide/Slide.js
@@ -0,0 +1,86 @@
+import styles from './Slide.scss';
+import Image from '~/components/Image/Image';
+import classNames from 'classnames/bind';
+
+import Carousel from 'react-material-ui-carousel';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+
+const cx = classNames.bind(styles);
+
+const SLIDES = [
+    {
+        title: '50% Off For Your First Shopping',
+        description:
+            'A Generic, extendible Carousel UI component for React using Material UI. It switches between given children using a smooth animation.',
+        image: 'https://bazaar.ui-lib.com/assets/images/products/nike-black.png',
+    },
+    {
+        title: '50% Off For Your First Shopping',
+        description:
+            'A Generic, extendible Carousel UI component for React using Material UI. It switches between given children using a smooth animation.',
+        image: 'https://bazaar.ui-lib.com/assets/images/products/nike-black.png',
+    },
+];
+
+function Slide() {
+    return (
+        <div className={cx('slide')}>
+            <Carousel
+                autoPlay={false}
+                animation="slide"
+                duration={400}
+                indicatorIconButtonProps={{
+                    style: {
+                        color: '#0F3460',
+                    },
+                }}
+                activeIndicatorIconButtonProps={{
+                    style: {
+                        border: '1px solid #0F3460',
+                    },
+                }}
+            >
+                {SLIDES.map((slide, index) => (
+                    <Grid
+                        key={index}
+                        className={cx('slide-item')}
+                        container
+                        spacing={3}
+                    >
+                        <Grid item xs={6}>
+                            <div className={cx('silde-item-body')}>
+                                <h3 className={cx('slide-title')}>
+                                    {slide.title}
+                                </h3>
+                                <span className={cx('slide-des')}>
+                                    {slide.description}
+                                </span>
+                                <Button
+                                    className={cx('shop-btn')}
+                                    sx={{
+                                        fontSize: '1.3rem',
+                                        fontWeight: '600',
+                                        color: 'var(--white)',
+                                        backgroundColor: 'var(--primary-color)',
+                                    }}
+                                >
+                                    Shop Now
+                                </Button>
+                            </div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Image
+                                className={cx('slide-img')}
+                                alt="Image Slide"
+                                src={slide.image}
+                            />
+                        </Grid>
+                    </Grid>
+                ))}
+            </Carousel>
+        </div>
+    );
+}
+
+export default Slide;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,17 @@
+import styles from './Home.scss';
+import Slide from '~/components/Slide/Slide';
+
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
 function Home() {
-    return <h2>Home page</h2>;
+    return (
+        <div className={cx('wrapper-home')}>
+            <Slide />
+            <div className={cx('home_container')}></div>
+        </div>
+    );
 }
 
 export default Home;
